77f1c230119e2ddfd6b1b5fabf5b78e8a0201d82
Vaibhav Dhyani <vd090904@gmail.com>
2025-06-01 08:40:03 +0530

    drawing board kinda

---
+++ etch_a_sketch.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+s = Screen()
+
+def moveForward():
+   t.forward(12)
+
+def moveBack():
+   t.backward(12)
+
+def turnLeft():
+   t.left(15)
+
+def turnRight():
+   t.right(15)
+
+def clrscr():
+   s.clearscreen()
+
+def right90():
+   t.right(90)
+
+def left90():
+   t.left(90)
+
+def Up():
+   t.up()
+
+def Down():
+   t.down()
+   
+
+
+
+s.listen()
+s.onkey(key="w", fun=moveForward)
+s.onkey(key="s", fun=moveBack)
+s.onkey(key="c", fun=clrscr)
+s.onkey(key="a", fun=turnLeft)
+s.onkey(key="d", fun=turnRight)
+s.onkey(key="r", fun=right90)
+s.onkey(key="r", fun=left90)
+s.onkey(key="u", fun=Up)
+s.onkey(key="o", fun=Down)
+s.exitonclick()
